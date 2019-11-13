e41e99f4c566bcb73f63e4971d37840f49a3114d
otodaceh <45671712+otodaceh@users.noreply.github.com>
2019-11-13 15:33:42 -0500

    Update check_udp_port.py

--- check_udp_port.py
+++ check_udp_port.py
@@ -13,6 +13,10 @@ def send_udp():
                      socket.SOCK_DGRAM) # UDP
     sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 def listen_for_icmp():
+    #Defines a listening socket to for ICMP - Closed UDP ports send an ICMP unreacheable message.
+    sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    
+    sock.bind(('local_ip_address', 1)) #Give it your computer's local IP.
     data = sock.recv(1024)
 
     # ip header is the first 20 bytes
@@ -28,13 +32,8 @@ def listen_for_icmp():
 '''
 MAIN
 '''
-#Defines a listening socket to for ICMP - Closed UDP ports send an ICMP unreacheable message.
-sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-sock.bind(('local_ip_address', 1)) #Give it your computer's local IP.
-
 try :
    while True :
-
     send_udp()
     listen_for_icmp()
       
